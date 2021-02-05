aa063cd06489cac8e9e21023ff4ce0d324a65d26
afsal1 <68735052+afsal1@users.noreply.github.com>
2021-02-05 23:02:08 +0530

    Update models.py

--- blog_app_sample/models.py
+++ blog_app_sample/models.py
@@ -7,3 +7,11 @@ class Blog(models.Model):
     title1 = models.CharField(max_length=255,null = True)
     content = models.TextField(null = True)
     content_image = models.ImageField(null = True, blank = True)
+    
+    @property
+    def imageURL(self):
+        try:
+            url = self.content_image.url
+        except:
+            url = ''
+        return url
